b51359cd6aa3537bd6a4440b93575dab09a383fa
Daniel Nguyen <blindkatana@gmail.com>
2011-03-27 13:06:27 -0700

    Starting project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crosslives::Application.config.secret_token = 'c0639860889295220668a9368265f14b5e2cd54710908d3852ea8c4cda52417992354339912b49575d6cd7667aabc05eff2fbb149897c6a533e709b1533ca359'
